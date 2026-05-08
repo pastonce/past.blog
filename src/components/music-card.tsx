3e39d142ca32e6538666f549803edc84900d8613
Gosh <150127315+pastonce@users.noreply.github.com>
2026-05-08 17:26:41 +0800

    更换主页bgm-补丁

--- src/components/music-card.tsx
+++ src/components/music-card.tsx
@@ -12,7 +12,7 @@ import { Pause } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
 
-const MUSIC_FILES = ['/music/close-to-you.mp3']
+const MUSIC_FILES = ['/music/HOYO-MiX - 宴语韵悠悠 Halcyon Times.mp3']
 
 export default function MusicCard() {
 	const pathname = usePathname()
